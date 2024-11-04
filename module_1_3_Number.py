fd787ba4035f2e919c544251d92a9af6b25e9ecd
Aleksandra305 <fktrcfylhf15389@gmail.com>
2024-11-04 14:33:13 +0300

    Add files via upload

---
+++ module_1_3_Number.py
@@ -0,0 +1,5 @@
+Number_tasks = 12
+Number_of_hours= 1.5
+Course_name = 'Python'
+Time_per_task = (90 / 12) / 60
+print('Курс:', Course_name, 'всего задач:',Number_tasks, 'затрачено часов:', Number_of_hours, 'среднее время выполнения', Time_per_task)
